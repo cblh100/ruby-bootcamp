65e7eaca3724641dd294cfd335b425cdc0e3b13e
Chris Leeds-Harrison <christopher.leeds-harrison@sky.uk>
2015-09-16 15:24:14 +0100

    Glued in the translation logic to the Linguine module and also added caching

--- exercises/rack/lib/linguine.rb
+++ exercises/rack/lib/linguine.rb
@@ -1,5 +1,7 @@
 require 'rack'
 require 'pry'
+require_relative 'oauth'
+require_relative 'ms_translator'
 
 module Linguine
 
@@ -8,25 +10,58 @@ module Linguine
   end
 
   module ClassMethods
-    attr_accessor :mappings
+
+    def mappings
+      @mappings ||= {}
+    end
 
     def page(uri, &block)
-      @mappings ||= {}
-      @mappings[uri] = block
+      mappings[uri] = block
     end
 
   end
 
+  def oauth
+    @oauth ||= OAuth.new('ddWDU0TuNZyoiAWtuhQn','Aez85ARdCZVpcKEKgOXOqBpai3SELO43MxdtI1Ml4KQ=')
+  end
+
+  def translator
+    @translator ||= MsTranslator.new(oauth)
+  end
+
+  def cache
+    @cache ||= {}
+  end
+
+  def add_to_cache(text, language, translated_text)
+    if cache[text]
+      cache[text][language] = translated_text
+    else
+      cache[text] = { language => translated_text }
+    end
+  end
+
   def call(env)
-    request_path = env['REQUEST_PATH']
+    request_path, language = split_path(env['REQUEST_PATH'])
     mappings = self.class::mappings
-    return [404, {"Content-Type" => "text/plain"}, "Oooops, there is nothing here" ] unless mappings && mappings[request_path]
-    [200, {"Content-Type" => "text/plain"}, mappings[request_path].call(env)]
+    return [404, {'Content-Type' => 'text/plain'}, 'Oooops, there is nothing here' ] unless mappings[request_path]
+
+    response_body = translate(mappings[request_path].call(env), language)
+
+    [200, {'Content-Type' => 'text/plain'}, response_body]
+  end
+
+  def translate(text, language)
+    return cache[text][language] if cache[text] && cache[text][language]
+
+    translated_text = translator.translate(text, 'en', language || 'en')
+    add_to_cache( text, language, translated_text)
+    translated_text
   end
 
   def split_path(request_path)
     /^(?<path>.*?)(\.(?<language>[a-z\-]+)){0,1}$/.match(request_path) do |m|
-      { :path => m['path'], :language => m['language'] }
+      [m['path'], m['language']]
     end
   end
 
--- exercises/rack/spec/linguine_spec.rb
+++ exercises/rack/spec/linguine_spec.rb
@@ -2,21 +2,21 @@ require 'spec_helper'
 
 describe Linguine do
 
-  let(:dummy_class) { Class.new { include Linguine } }
-  let(:dummy_object) { dummy_class.new }
+  let(:linguine_class) { Class.new { include Linguine } }
+  let(:linguine_object) { linguine_class.new }
 
   describe '::page' do
 
     it 'empty mappings hash' do
-      expect(dummy_class.mappings).to be_nil
+      expect(linguine_class.mappings).to be_empty
     end
 
     it 'store and call block' do
-      dummy_class.page '/' do
+      linguine_class.page '/' do
         'Dummy Output'
       end
-      expect(dummy_class.mappings).to include('/')
-      expect(dummy_class.mappings['/'].call).to eq('Dummy Output')
+      expect(linguine_class.mappings).to include('/')
+      expect(linguine_class.mappings['/'].call).to eq('Dummy Output')
     end
 
   end
@@ -24,16 +24,90 @@ describe Linguine do
   describe '#split_path' do
 
     it 'hash with only the path' do
-      expect(dummy_object.split_path('/about')).to eq( { :path => '/about', :language => nil } )
+      expect(linguine_object.split_path('/about')).to eq( ['/about', nil] )
     end
 
     it 'hash with path and the language' do
-      expect(dummy_object.split_path('/about.de')).to eq( { :path => '/about', :language => 'de' } )
+      expect(linguine_object.split_path('/about.de')).to eq(%w(/about de))
     end
 
     it 'hash with path and the extended language' do
-      expect(dummy_object.split_path('/about.en-gb')).to eq( { :path => '/about', :language => 'en-gb' } )
+      expect(linguine_object.split_path('/about.en-gb')).to eq(%w(/about en-gb))
     end
+
+  end
+
+  describe '#cache' do
+    it 'the cache starts empty' do
+      expect(linguine_object.cache).to be_empty
+    end
+  end
+
+  describe '#add_to_cache' do
+    it 'one translation can be added to the cache' do
+      linguine_object.add_to_cache( 'hello world', 'de', 'hallo welt' )
+      expect(linguine_object.cache).to eq({ 'hello world' => { 'de' => 'hallo welt' } })
+    end
+
+    it 'two translations for the same text can be added to the cache' do
+      linguine_object.add_to_cache( 'hello world', 'de', 'hallo welt' )
+      linguine_object.add_to_cache( 'hello world', 'fr', 'bonjour le monde' )
+      expect(linguine_object.cache).to eq({ 'hello world' => { 'de' => 'hallo welt', 'fr' => 'bonjour le monde' } })
+    end
+
+    it 'two translations for different text can be added to the cache' do
+      linguine_object.add_to_cache( 'hello world 1', 'de', 'hallo welt 1' )
+      linguine_object.add_to_cache( 'hello world 2', 'fr', 'bonjour le monde 2' )
+      expect(linguine_object.cache).to eq({ 'hello world 1' => { 'de' => 'hallo welt 1' }, 'hello world 2' => {  'fr' => 'bonjour le monde 2' } })
+    end
+  end
+
+  describe '#translate' do
+    context 'translation is already in the cache' do
+      let(:text_to_translate) { 'hello world' }
+      let(:translated_text) { 'hallo welt (from the cache)' }
+      let(:language) { 'de' }
+
+      it 'the cached value is returned' do
+        linguine_object.add_to_cache(text_to_translate, language, translated_text)
+        allow(linguine_object).to receive(:translator)
+        expect(linguine_object).not_to have_received(:translator)
+        expect(linguine_object.translate(text_to_translate, language)). to eq(translated_text)
+      end
+    end
+
+    context 'translation text is already in the cache but not for this language' do
+      let(:text_to_translate) { 'hello world' }
+      let(:translated_text) { 'bonjour le monde' }
+
+      it 'the translated value is returned' do
+        translator = double()
+        allow(translator).to receive(:translate).and_return(translated_text)
+        linguine_object.add_to_cache(text_to_translate, 'de', 'hallo welt')
+        allow(linguine_object).to receive(:translator).and_return(translator)
+        expect(linguine_object.translate(text_to_translate, 'fr')). to eq(translated_text)
+      end
+    end
+
+    context 'translation text is not in the cache' do
+      let(:text_to_translate) { 'hello world' }
+      let(:translated_text) { 'bonjour le monde' }
+      let(:language) { 'fr' }
+
+      it 'the translated value is returned' do
+        translator = double()
+        allow(translator).to receive(:translate).and_return(translated_text)
+        allow(linguine_object).to receive(:translator).and_return(translator)
+        expect(linguine_object.translate(text_to_translate, language)). to eq(translated_text)
+        expect(linguine_object.cache).to eq({ text_to_translate => { language => translated_text } })
+      end
+    end
+
+  end
+
+  describe '#call' do
+
+
 
   end
 
